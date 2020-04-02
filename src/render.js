a35bd01dde701fa9b7c669e90985b6039862d106
Andrew <andrewdavis1191@gmail.com>
2020-04-02 00:52:14 -0700

    Adding boilerplate, sqlite, some js

---
+++ src/render.js
@@ -0,0 +1,55 @@
+
+
+// Buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+// Get the available video sources
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
+
+  videoOptionsMenu.popup();
+
+}
+
+// Change the videoSource window to record
+async function selectSoruce(source) {
+
+  videoSelectBtn.InnerText = source.name
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints);
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
+};
