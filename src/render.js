bdbfc6538077cdb1f144ee7e6a404bac366f2cc1
Andrew <andrewdavis1191@gmail.com>
2020-04-02 01:31:12 -0700

    Changing some ids and adding a delete button

--- src/render.js
+++ src/render.js
@@ -1,28 +1,9 @@
-// SqlLite Stuff
-let sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(':memory:');
-
-db.serialize(function() {
-  db.run("CREATE TABLE users (info TEXT)");
-
-  let stmt = db.prepare("INSERT INTO users VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM users", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
-
-db.close();
-
-
-// Buttons
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
-videoSelectBtn.onclick = getVideoSources;
-
 const { fs, remote } = require('electron');
 const { Menu } = remote;
+
+// Function here to perform on-click operations
+
+// Buttons
+const addUserBtn = document.getElementById('add-user-button');
+const deleteUserBtn = document.getElementById('delete-user-button');
+addUserBtn.onclick = getVideoSources;
